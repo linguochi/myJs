a7a017406c87416ffa30698e7d8000c26880aa58
linguochi <382150914@qq.com>
2016-08-10 16:05:30 +0800

    滚到顶部部件

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,35 +1,52 @@
 /**
- * Created by lingc on 2016/8/6.
+ * 滚到顶部部件
+ * Created by lingc on 2016/8/10.
+ * https://github.com/linguochi/myJs
  */
-$.fn.scrolltotop = function (options) {
-    var btn = $(this);
+(function ($, window) {
     var defaults = {
-        startshow: 100,
-        speed: 20
+        offset: 300,
+        offset_opacity: 1200,
+        scroll_top_duration: 700, //滑动时间
+        $back_to_top: $('.cd-top')
     };
-    var opts = $.extend(defaults, options);
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > defaults.startshow) {
-            btn.fadeIn();
-        } else {
-            btn.fadeOut();
+    var ScrollToTop = function ($element, options) {
+        this.$element = $element;
+        this.settings = $.extend({}, defaults, options);
+        this.init();
+    };
+    ScrollToTop.prototype = {
+        init: function () {
+            this.bindListener();
+        },
+        bindListener: function () {
+            var self = this;
+            //hide or show the "back to top" link
+            $(window).scroll(function () {
+                ( $(this).scrollTop() > self.settings.offset ) ? self.settings.$back_to_top.addClass('cd-is-visible') : self.settings.$back_to_top.removeClass('cd-is-visible cd-fade-out');
+                if ($(this).scrollTop() > self.settings.offset_opacity) {
+                    self.settings.$back_to_top.addClass('cd-fade-out');
+                }
+            });
+            //滑动更顺滑~~~~~
+            self.settings.$back_to_top.on('click', function (event) {
+                event.preventDefault();
+                $('body,html').animate({
+                        scrollTop: 0,
+                    }, self.settings.scroll_top_duration
+                );
+            });
         }
-    });
-    btn.click(function () {
-        var t = $(window).scrollTop();
-        var k = 0;
-
-        timer = setInterval(function () {
-            h = Math.ceil(0.1 * (t - k));
-            k = k + h;
-            j = t - k;
-            $(window).scrollTop(j);
-
-            if (j == 0) {
-                clearInterval(timer);
-                btn.fadeOut();
+    };
+    $.fn.scrollToTop = function (m) {
+        return this.each(function () {
+            var $this = $(this),
+                data = $this.data('scrollToToper');
+            if (data && data.scrollToToper) {
+                return;
             }
-        }, defaults.speed);
-    });
-
-}
+            data = $this.data('scrollToToper', true);
+            var scrollToTop = new ScrollToTop($this, m);
+        });
+    };
+})(jQuery, window);
